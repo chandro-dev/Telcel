d17e3fa73815ad5acdb7a9361d8124f830f15564
alejandro <alejocarreteroballesteros@gmail.com>
2023-03-27 15:34:05 -0500

    V0.0.1 |27/03/2023 |Ananias

---
+++ Telcel/Pages/Principal.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas.Pages
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Page
+    {
+        public Principal()
+        {
+            InitializeComponent();
+            metodo();
+        }
+        public void metodo()
+        {
+            page pagina = new page();
+            this.NavigationService.Navigate(pagina);
+        }
+    }
+}
